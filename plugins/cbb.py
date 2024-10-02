eaf92d0708169c2f916ce902167366018be74685
RNGDebeloper <131392755+RNGDebeloper@users.noreply.github.com>
2024-10-02 23:30:28 +0530

    Update cbb.py
    Dd

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,11 +10,12 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b> Heyy! {first},\nOur Premium package offers several benefits, including:</b>\n\n• Direct channel links with no ad links.\n• On-demand collections and one daily request.\n• Special access to events.\n\n<b>Included Channels In This Plan:</b>\n• @JAV_Uncensoreds\n• @HentaiHomies\n• @Live_Action_Hentai_1\n• @nHentai_Mangas\n• @OnlyFans_Adult_Archive\n• @PornHub_Premium_Videoss\n• @Ongoing_hanimes Coming soon..\n\n<b>Pricing:</b>\n1 Month - $1\n3 Months - $5\n6 Months - $10\n9 Months - $15\n12 Months - $20\n\n<b>Payment Methods:</b>\n• Payeer Account: P1109704654\n• Crypto: DM : @Its_Sasuke_Uchiha For Address.\n\nIf you are interested in subscribing, Send a payment screenshot to @Its_Sasuke_Uchiha. (For Process Auto Verification)\n\n<b>Please note that this pricing is temporary and we will soon be increasing our prices, We also have limited seats available for Premium users.</b>",
+            text = f"<b> Heyy! Buddy 😎\nOur Premium package offers several benefits, including:</b>\n\n• Direct channel links with no ad links.\n• On-demand collections and one daily request.\n• Special access to events.\n\n<b>Included Channels In This Plan:</b>\n• @JAV_Uncensoreds\n• @HentaiHomies\n• @Live_Action_Hentai_1\n• @nHentai_Mangas\n• @OnlyFans_Adult_Archive\n• @PornHub_Premium_Videoss\n• @Ongoing_hanimes Coming soon..\n\n<b>Pricing:</b>\n1 Month - $1\n3 Months - $5\n6 Months - $10\n9 Months - $15\n12 Months - $20\n\n<b>Payment Methods:</b>\n• Payeer Account: P1109704654\n• Crypto: DM : @Its_Sasuke_Uchiha For Address.\n\nIf you are interested in subscribing, Send a payment screenshot to @Its_Sasuke_Uchiha. (For Process Auto Verification)\n\n<b>Please note that this pricing is temporary and we will soon be increasing our prices, We also have limited seats available for Premium users.</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
+                        InlineKeyboardButton("List Of All Channels", url = "https://t.me/Uchiha_Community/40"),
                         InlineKeyboardButton("🔒 Close", callback_data = "close")
                     ]
                 ]
