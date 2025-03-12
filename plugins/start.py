167aefd6615156a9ec8007232f26597303152994
RNGDebeloper <131392755+RNGDebeloper@users.noreply.github.com>
2025-03-12 15:45:03 +0530

    Update start.py
    dd

--- plugins/start.py
+++ plugins/start.py
@@ -137,7 +137,7 @@ REPLY_ERROR = """<code>Use this command as a replay to any telegram message with
 async def not_joined(client: Client, message: Message):
     buttons = [
         [
-                    InlineKeyboardButton("Join Channel", url = "https://t.me/+le0s_vv5tnFmZmZl"),
+                    InlineKeyboardButton("Join Channel", url = "https://t.me/+-9iVoOPHZZUxNDQ1"),
                     InlineKeyboardButton("Join 2nd Channel", url = "https://t.me/+78eMfVRhnu9mZmNl")
                     
         ],
